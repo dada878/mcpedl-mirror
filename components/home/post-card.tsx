d434c143062f9e5a74da90a3369c3ba273872081
Dada878 <xx0932399@gmail.com>
2024-05-28 16:42:19 +0800

    make post card footer always bottom

--- components/home/post-card.tsx
+++ components/home/post-card.tsx
@@ -12,18 +12,25 @@ import Link from "next/link";
 export default function PostCard({ post }: { post: Post }) {
   return (
     <Link href={post.link} target="_blank" className="block w-full h-full">
-      <Card className="w-82 flex flex-col justify-between h-full hover:bg-muted cursor-pointer transition">
-      <CardHeader>
-        <Image src={post.image} alt={post.title} width={400} height={200} />
-      </CardHeader>
-      <CardContent>
-        <CardTitle className="text-xl">{post.index} {post.title}</CardTitle>
-        <CardDescription>{post.description}</CardDescription>
-      </CardContent>
-      <CardFooter>
-        <small>Publish date {post.date.toISOString().split("T")[0].replaceAll("-", " / ")}</small>
-      </CardFooter>
-    </Card>
+      <Card className="w-82 flex flex-col h-full hover:bg-muted cursor-pointer transition justify-between">
+        <div>
+          <CardHeader>
+            <Image src={post.image} alt={post.title} width={400} height={200} />
+          </CardHeader>
+          <CardContent>
+            <CardTitle className="text-xl">
+              {post.index} {post.title}
+            </CardTitle>
+            <CardDescription>{post.description}</CardDescription>
+          </CardContent>
+        </div>
+        <CardFooter>
+          <small>
+            Publish date{" "}
+            {post.date.toISOString().split("T")[0].replaceAll("-", " / ")}
+          </small>
+        </CardFooter>
+      </Card>
     </Link>
   );
 }
