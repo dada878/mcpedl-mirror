aff8c1c6101079ea31b337b779e31d05712e7f4b
Dada878 <xx0932399@gmail.com>
2024-05-28 15:14:42 +0800

    add mcpedl image domain to whitelist

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: "api.mcpedl.com"
+      }
+    ]
+  },};
 
 export default nextConfig;
