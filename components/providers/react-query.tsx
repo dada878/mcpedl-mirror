d6185809723e2c54c5f0427747cdc193dcdb259b
Dada878 <xx0932399@gmail.com>
2024-05-28 16:07:25 +0800

    format react-query.tsx

--- components/providers/react-query.tsx
+++ components/providers/react-query.tsx
@@ -1,18 +1,17 @@
-"use client"
+"use client";
 
-import {
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
 export default function QueryProvider({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+    </QueryClientProvider>
   );
 }
