9ad23fd618ce5e3d7fafa928877266a9baa77cd1
Dada878 <xx0932399@gmail.com>
2024-05-28 21:02:16 +0800

    support fetch post with more categories

--- app/api/refresh/route.ts
+++ app/api/refresh/route.ts
@@ -1,77 +1,102 @@
 import { db } from "@/lib/firebase-admin";
 import jsdom from "jsdom";
 
-const url = "https://mcpedl.com/category/mods/";
+const urls = [
+  {
+    url: "https://mcpedl.com/category/mods/",
+    type: "addon",
+  },
+  {
+    url: "https://mcpedl.com/category/maps/",
+    type: "map",
+  },
+  {
+    url: "https://mcpedl.com/category/texture-packs/",
+    type: "texture",
+  },
+];
 
 export async function GET() {
-  const result = await (await fetch(url)).text();
-  const dom = new jsdom.JSDOM(result);
-  const elements = dom.window.document.querySelectorAll("div.fancybox.post");
-
-  const lastQuerySnapshot = await db.collection("posts").orderBy("index", "desc").limit(1).get();
-  let lastIndex = lastQuerySnapshot.docs.length > 0 ? lastQuerySnapshot.docs[0].data().index : 0;
-
   const posts = [];
 
-  for (const element of Array.from(elements)) {
-    const titleElement = element.querySelector(".fancybox__content__title > a");
-    const title = titleElement ? titleElement.textContent : "No title";
-    const link = titleElement?.getAttribute("href")
-      ? titleElement.getAttribute("href")
-      : "No link";
-    const id = link ? link.replaceAll("/", "") : "No link";
-    const imageElement = element.querySelector(
-      ".post__img__static.cursor-pointer img"
-    );
-    const image = imageElement?.getAttribute("src")
-      ? imageElement.getAttribute("src")
-      : "/no-image.png";
-    const publishDateElement = element.querySelector(
-      ".fancybox__header__content small"
-    );
-    const publishDate = publishDateElement
-      ? publishDateElement.textContent?.replaceAll("Published on ", "").trim()
-      : "No publish date";
+  for (const { url, type } of urls) {
+    const result = await (await fetch(url)).text();
+    const dom = new jsdom.JSDOM(result);
+    const elements = dom.window.document.querySelectorAll("div.fancybox.post");
 
-    const descriptionElement = element.querySelector(
-      ".fancybox__content__description.cursor-pointer"
-    );
-    const description = descriptionElement?.textContent
-      ? descriptionElement.textContent.trim()
-      : "No description";
+    const lastQuerySnapshot = await db
+      .collection("posts")
+      .orderBy("index", "desc")
+      .where("type", "==", type)
+      .limit(1)
+      .get();
+    let lastIndex =
+      lastQuerySnapshot.docs.length > 0
+        ? lastQuerySnapshot.docs[0].data().index
+        : 0;
 
-    if (
-      publishDate === "No publish date" ||
-      link === "No link" ||
-      id === "No link" ||
-      title === "No title" ||
-      image === "/no-image.png" ||
-      description === "No description" ||
-      !publishDate ||
-      !link ||
-      !id ||
-      !title ||
-      !image ||
-      !description
-    ) {
-      continue;
-    }
+    for (const element of Array.from(elements)) {
+      const titleElement = element.querySelector(
+        ".fancybox__content__title > a"
+      );
+      const title = titleElement ? titleElement.textContent : "No title";
+      const link = titleElement?.getAttribute("href")
+        ? titleElement.getAttribute("href")
+        : "No link";
+      const id = link ? link.replaceAll("/", "") : "No link";
+      const imageElement = element.querySelector(
+        ".post__img__static.cursor-pointer img"
+      );
+      const image = imageElement?.getAttribute("src")
+        ? imageElement.getAttribute("src")
+        : "/no-image.png";
+      const publishDateElement = element.querySelector(
+        ".fancybox__header__content small"
+      );
+      const publishDate = publishDateElement
+        ? publishDateElement.textContent?.replaceAll("Published on ", "").trim()
+        : "No publish date";
 
-    posts.push({
-      title,
-      id,
-      link: `https://mcpedl.com${link}`,
-      image,
-      date: new Date(publishDate),
-      description,
-      index: ++lastIndex,
-      type: "addon"
-    });
+      const descriptionElement = element.querySelector(
+        ".fancybox__content__description.cursor-pointer"
+      );
+      const description = descriptionElement?.textContent
+        ? descriptionElement.textContent.trim()
+        : "No description";
 
-    for (const post of posts) {
-      const existingPost = await db.collection("posts").doc(post.id).get();
-      if (!existingPost.exists) {
-        db.collection("posts").doc(post.id).set(post, { merge: true });
+      if (
+        publishDate === "No publish date" ||
+        link === "No link" ||
+        id === "No link" ||
+        title === "No title" ||
+        image === "/no-image.png" ||
+        description === "No description" ||
+        !publishDate ||
+        !link ||
+        !id ||
+        !title ||
+        !image ||
+        !description
+      ) {
+        continue;
+      }
+
+      posts.push({
+        title,
+        id,
+        link: `https://mcpedl.com${link}`,
+        image,
+        date: new Date(publishDate),
+        description,
+        index: ++lastIndex,
+        type,
+      });
+
+      for (const post of posts) {
+        const existingPost = await db.collection("posts").doc(post.id).get();
+        if (!existingPost.exists) {
+          db.collection("posts").doc(post.id).set(post, { merge: true });
+        }
       }
     }
   }
