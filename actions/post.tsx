2dd963bc67301e9aa3b13565d868b8c72e8dc8b9
Dada878 <xx0932399@gmail.com>
2024-05-28 19:42:01 +0800

    add save button to post card

--- actions/post.tsx
+++ actions/post.tsx
@@ -1,8 +1,52 @@
 "use server";
 
+import { authOptions } from "@/lib/auth";
 import { db } from "@/lib/firebase-admin";
+import { getServerSession } from "next-auth";
 
-export async function getPosts(page: number = 1, type: string): Promise<Post[]> {
+export async function removeSavedPost(postId: string) {
+  const session = await getServerSession(authOptions);
+  if (!session || !session.user) {
+    return;
+  }
+  const snapshot = await db
+    .collection("saved")
+    .where("postId", "==", postId)
+    .where("userId", "==", session.user.id)
+    .get();
+  snapshot.docs.forEach((doc) => {
+    doc.ref.delete();
+  });
+}
+
+export async function savePost(postId: string) {
+  const session = await getServerSession(authOptions);
+  if (!session || !session.user) {
+    return;
+  }
+  await db.collection("saved").add({
+    postId,
+    userId: session.user.id,
+  });
+}
+
+export async function isSaved(postId: string) {
+  const session = await getServerSession(authOptions);
+  if (!session || !session.user) {
+    return false;
+  }
+  const snapshot = await db
+    .collection("saved")
+    .where("postId", "==", postId)
+    .where("userId", "==", session.user.id)
+    .get();
+  return snapshot.docs.length > 0;
+}
+
+export async function getPosts(
+  page: number = 1,
+  type: string
+): Promise<Post[]> {
   const lastQuerySnapshot = await db
     .collection("posts")
     .where("type", "==", type)
--- components/home/post-card.tsx
+++ components/home/post-card.tsx
@@ -1,3 +1,4 @@
+import { isSaved, savePost, removeSavedPost } from "@/actions/post";
 import {
   Card,
   CardContent,
@@ -6,31 +7,80 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { Star } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
 export default function PostCard({ post }: { post: Post }) {
+  const { data: saved } = useQuery({
+    queryKey: ["is-saved", post.id],
+    queryFn: () => isSaved(post.id),
+  });
+
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation({
+    mutationFn: async () => {
+      if (saved) {
+        await removeSavedPost(post.id);
+      } else {
+        await savePost(post.id);
+      }
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["is-saved", post.id],
+      });
+    },
+    onMutate: () => {
+      queryClient.setQueryData(["is-saved", post.id], !saved);
+    },
+    onError: () => {
+      queryClient.setQueryData(["is-saved", post.id], saved);
+    },
+  });
+
   return (
-    <Link href={post.link} target="_blank" className="block w-full h-full">
-      <Card className="w-82 flex flex-col h-full hover:bg-muted cursor-pointer transition justify-between">
-        <div>
-          <CardHeader>
-            <Image src={post.image} alt={post.title} width={400} height={200} />
-          </CardHeader>
-          <CardContent>
-            <CardTitle className="text-xl">
-              {post.index} {post.title}
-            </CardTitle>
-            <CardDescription>{post.description}</CardDescription>
-          </CardContent>
+    <Card className="w-82 flex flex-col h-full transition justify-between">
+      <Link href={post.link} target="_blank" className="block w-full h-full">
+        <CardHeader>
+          <Image
+            src={post.image}
+            alt={post.title}
+            width={400}
+            height={200}
+            className="w-full"
+          />
+        </CardHeader>
+        <CardContent>
+          <CardTitle className="text-xl">{post.title}</CardTitle>
+          <CardDescription>{post.description}</CardDescription>
+        </CardContent>
+      </Link>
+      <CardFooter className="flex gap-6 items-center cursor-default">
+        <small className="text-nowrap whitespace-nowrap">
+          {post.date.toISOString().split("T")[0].replaceAll("-", " / ")}
+        </small>
+        <div className="flex justify-end gap-2 w-full">
+          <div
+            onClick={() => {
+              mutation.mutate();
+            }}
+          >
+            {saved ? (
+              <Star
+                size={26}
+                className="cursor-pointer"
+                fill="#ffd363"
+                color="#f3ab4e"
+              />
+            ) : (
+              <Star size={26} className="cursor-pointer" />
+            )}
+          </div>
         </div>
-        <CardFooter>
-          <small>
-            Publish date{" "}
-            {post.date.toISOString().split("T")[0].replaceAll("-", " / ")}
-          </small>
-        </CardFooter>
-      </Card>
-    </Link>
+      </CardFooter>
+    </Card>
   );
 }
