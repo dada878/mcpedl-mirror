1fa5239e638a45e416e3004fdf5000389ecf4774
Dada878 <xx0932399@gmail.com>
2024-05-28 13:58:57 +0800

    setup next-auth

---
+++ lib/auth.ts
@@ -0,0 +1,25 @@
+import { FirestoreAdapter } from "@auth/firebase-adapter";
+import GoogleProvider from "next-auth/providers/google";
+import { cert } from "firebase-admin/app";
+import { NextAuthOptions } from "next-auth";
+import { Adapter } from "next-auth/adapters";
+
+export const authOptions: NextAuthOptions = {
+  adapter: FirestoreAdapter({
+    credential: cert({
+      projectId: process.env.AUTH_FIREBASE_PROJECT_ID,
+      clientEmail: process.env.AUTH_FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.AUTH_FIREBASE_PRIVATE_KEY!.replaceAll("\\n", "\n"),
+    }),
+  }) as Adapter,
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: 'jwt'
+  },
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+    })
+  ],
+};
