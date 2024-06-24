099f25d036708432c741b05829182c027c800abd
Dada878 <xx0932399@gmail.com>
2024-06-24 17:57:45 +0800

    update seo config

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,4 @@
 import InfiniteLoader from "@/components/home/infinite-post-loader";
-import { defaultSEO } from "@/lib/seo";
-import { Metadata } from "next";
-
-export const metadata: Metadata = defaultSEO();
 
 export default function Home() {
   return (
